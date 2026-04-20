6b0065ed58d4b8eca35b272c67df4019329c267e
francisgensheimer123-wq <francis.gensheimer123@gmail.com>
2026-04-20 12:59:51 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -122,7 +122,7 @@ if os.environ.get("DATABASE_URL"):
         "default": dj_database_url.parse(
             os.environ.get("DATABASE_URL"),
             conn_max_age=600,
-            conn_health_checks=True,
+            conn_health_checks=False,
         )
     }
 
